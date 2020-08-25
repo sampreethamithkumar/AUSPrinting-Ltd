02da52cd19f55adc45b860ef583bdf0b6f75f0a6
sampreeth65 <55499513+sampreeth65@users.noreply.github.com>
2020-08-25 23:26:37 +1000

    Web interface development

---
+++ AUSPrinting/src/fit5042/assx/entities/Customer.java
@@ -0,0 +1,12 @@
+package fit5042.assx.entities;
+
+public class Customer 
+{
+	private int customerId;
+	private String customerFirstName;
+	private String customerLastName;
+	private String customerDob;
+	private String customerIndustry;
+	
+	
+}
