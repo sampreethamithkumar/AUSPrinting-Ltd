1b8607d125bc2bfa9c00da5a003f798bdc5c55a5
sami0013 <sami0013@student.monash.edu>
2020-10-26 12:11:47 +1100

    minor changes

--- AUSPrinting/src/fit5042/assx/entities/Users.java
+++ AUSPrinting/src/fit5042/assx/entities/Users.java
@@ -6,10 +6,13 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "Users")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Users implements Serializable{
 	
 	private long id;
